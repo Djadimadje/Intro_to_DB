694972f2eeda65dc8d1d5f44f5aeba23e032e6b0
Djadimadje <m.djadimadje@gmail.com>
2025-02-01 15:17:39 -0800

    Modify task_4.sql

--- task_4.sql
+++ task_4.sql
@@ -1,13 +1,13 @@
--- Retrieve the full description of the books table using INFORMATION_SCHEMA.COLUMNS
-SELECT 
-    COLUMN_NAME, 
-    COLUMN_TYPE, 
-    IS_NULLABLE, 
-    COLUMN_KEY, 
-    COLUMN_DEFAULT, 
-    EXTRA 
-FROM 
-    INFORMATION_SCHEMA.COLUMNS 
-WHERE 
-    TABLE_SCHEMA = 'alx_book_store' 
-    AND TABLE_NAME = 'books';
+-- Retrieve the full description of the Books table using INFORMATION_SCHEMA.COLUMNS
+SELECT
+    COLUMN_NAME,
+    COLUMN_TYPE,
+    IS_NULLABLE,
+    COLUMN_KEY,
+    COLUMN_DEFAULT,
+    EXTRA
+FROM
+    INFORMATION_SCHEMA.COLUMNS
+WHERE
+    TABLE_SCHEMA = 'alx_book_store'
+    AND TABLE_NAME = 'Books';
